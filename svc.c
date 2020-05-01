56d628940e38b57cf54fd48cec9661f34819504e
xinchen <xzha9245@uni.sydney.edu.au>
2020-05-01 11:38:17 +1000

    135

--- svc.c
+++ svc.c
@@ -946,13 +946,13 @@ char *svc_merge(void *helper, char *branch_name, struct resolution *resolutions,
 
     //consturct the message
 
-    // char message[14+50+1] = {0};
-    // char prefix[15] = "Merged branch ";
-    // sprintf(message, "%s", prefix);
-    // sprintf(message+14, "%s", branch_name);
+    char message[14+50+1] = {0};
+    char prefix[15] = "Merged branch ";
+    sprintf(message, "%s", prefix);
+    sprintf(message+14, "%s", branch_name);
     // printf("length of the message is %ld\n", 14+strlen(branch_name));
-    // message[14+strlen(branch_name)] = '\0';
-    char message[5] = "test";
+    message[strlen(message)] = '\0';
+    // char message[5] = "test";
 
     //Make the new commit from the stage
     commit_t *prev[2] = {current_commit, merged_branch_commit};
